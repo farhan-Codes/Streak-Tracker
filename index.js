0c9bc88ad191dc9149ca2257db591747f123b27e
Farhan Danish <codesbyfarhan@gmail.com>
2023-12-31 15:08:52 +0530

    Created a Data updater function and removed id from all cards

---
+++ index.js
@@ -0,0 +1,21 @@
+function randomPerc(pg) {
+    let rval = Math.floor(Math.random()*284);
+    let vperc = (rval/284)*100;
+    let bval = Math.abs((vperc*284)/100-284);
+    pg.querySelector('.per').innerText=`${Math.round(vperc)}%`;
+    pg.querySelector('.vcircle').style.strokeDashoffset=`${bval}px`;
+}
+
+let gyp = document.querySelector('#ygc');
+let gmp = document.querySelector('#mgc');
+let gtp = document.querySelector('#dgc');
+let syp = document.querySelector('#ysc');
+let smp = document.querySelector('#msc');
+let stp = document.querySelector('#dsc');
+
+randomPerc(gyp)
+randomPerc(gmp)
+randomPerc(gtp)
+randomPerc(syp)
+randomPerc(smp)
+randomPerc(stp)
